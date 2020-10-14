ca541059c491d79703115961452616bc2f0cc652
RileyL <51372465+gundamace17@users.noreply.github.com>
2020-10-14 12:10:46 -0700

    Update Quiz1 Question1.cpp

--- Quiz1 Question1.cpp
+++ Quiz1 Question1.cpp
@@ -22,3 +22,19 @@ int main()
     cout << f(1) << endl;
     return 0;
 }
+
+/*
+
+               f(1) <-|
+               /  \   | back to the previous call
+              /    \  |
+            n=2    f(2) <--|
+            i=2    /   \   | back to the previous call
+                  /     \  |
+                n=4     f(4) <--------------|
+                i=3     /   \               |
+                       /     \              |
+                     n=7     f(7) -> return 7 and go back to the previous call
+                     i=4
+                  
+*/
