307a078726d141d38365e882a73fc716f2db9217
RileyL <51372465+gundamace17@users.noreply.github.com>
2020-10-14 15:29:24 -0700

    Update Quiz1 Question3.cpp

--- Recursion/Quiz1 Question3.cpp
+++ Recursion/Quiz1 Question3.cpp
@@ -20,3 +20,29 @@ int main()
     return 0;
 }
 
+/*
+o/p: 1*9*9*9*9 = 6561
+Expalanation of the Algorithm
+
+Because the first pararmeter is passed by reference,
+the x value will be what the last call used, 9.
+
+              multiply by x=9
+      f(5, 5) <----|
+     /       \     |
+    /         \    |   multiply by x=9
+c=4, x=6       f(6, 4) <----|
+              /       \     |
+             /         \    |   multiply by x=9
+        c=3, x=7        f(7, 3) <----|
+                       /       \     |
+                      /         \    |    multiply by x=9
+                 c=2, x=8        f(8, 2) <----------|
+                                /       \           |
+                               /         \          |
+                          c=1, x=9        f(9, 1)->return 1
+                                         /       \
+                                        /         \
+                                      c=0          No more fun call
+     
+*/
