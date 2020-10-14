8d36f30b517d699e0ed7c443c3b6cda6078a65db
RileyL <51372465+gundamace17@users.noreply.github.com>
2020-10-14 11:55:11 -0700

    Add files via upload

---
+++ Quiz1 Question1.cpp
@@ -0,0 +1,24 @@
+// Quiz1 Question1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int f(int n)
+{
+    static int i = 1;
+    if (n >= 5)
+    {
+        return n;
+    }
+    n += i;
+    i++;
+    return f(n);
+}
+
+int main()
+{
+    cout << f(1) << endl;
+    return 0;
+}
